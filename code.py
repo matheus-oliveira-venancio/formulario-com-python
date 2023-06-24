d75af7510c5b0086b101e935da294ee56f17b88b
Matheus Oliveira <112136868+matheus-oliveira-venancio@users.noreply.github.com>
2023-06-24 19:46:06 -0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,6 @@
+#today we are going to create a form program with pyton
+
+name = input ("what is your name")
+old = input ("how old are you")
+song = input ("what is your fav song")
+print ('hi your name is', name, 'you have', old, 'years old', 'your favourite song is', song)
